2856c60fe2faf3f14aa8c53f5ca25afa8dcdb98d
harbinger965 <johnfiorentino@lawn-128-61-28-27.lawn.gatech.edu>
2017-01-27 11:35:31 -0500

    Model Templates
    Test Templates - Usage not confirmed

---
+++ HouseKeeper/HouseKeeper/HomeModel.swift
@@ -0,0 +1,101 @@
+//
+//  Model.swift
+//  HouseKeeper
+//
+//  Created by John Fiorentino on 1/26/17.
+//  Copyright © 2017 Profectus. All rights reserved.
+//
+
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+import Foundation
+
+protocol HomeModelProtocal: class {
+    func itemsDownloaded(items: NSArray)
+}
+
+
+class HomeModel: NSObject, NSURLSessionDataDelegate {
+    
+    //properties
+    
+    weak var delegate: HomeModelProtocal!
+    
+    var data : NSMutableData = NSMutableData()
+    
+    let urlPath: String = "http://iosquiz.com/service.php" //this will be changed to the path where service.php lives
+    
+    
+    func downloadItems() {
+        
+        let url: NSURL = NSURL(string: urlPath)!
+        var session: NSURLSession!
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        
+        session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        
+        let task = session.dataTaskWithURL(url)
+        
+        task.resume()
+        
+    }
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+        self.data.appendData(data);
+        
+    }
+    
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        if error != nil {
+            print("Failed to download data")
+        }else {
+            print("Data downloaded")
+            self.parseJSON()
+        }
+        
+    }
+}
